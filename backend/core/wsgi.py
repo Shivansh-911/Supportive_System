f8c91c7cd85b2e405b3ddb5a8e1bae47d01d459a
Shivansh Porwal <111256068+Shivansh-911@users.noreply.github.com>
2026-07-06 11:30:37 +0530

    Update wsgi.py

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,14 +1,5 @@
 import os
 
-try:
-    import newrelic.agent
-    newrelic.agent.initialize(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), 'newrelic.ini'),
-        os.environ.get('DJANGO_ENV', 'development'),
-    )
-except ModuleNotFoundError:
-    pass
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
